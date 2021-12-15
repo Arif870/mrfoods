f96b6453529788ebb80ac0213f8323e3884bee62
Arif870 <arifuzzaman959@gmail.com>
2021-12-15 21:13:11 +0600

    all set and header part done

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Map = () => {
+  return (
+    <div>
+      <h1>MAP</h1>
+    </div>
+  );
+};
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const PlaceDetails = () => {
+  return (
+    <div>
+      <h1>Place details</h1>
+    </div>
+  );
+};
